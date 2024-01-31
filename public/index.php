6cbcfb0907602bbfe3c152443011cf6532cea259
Kolia913 <mynew1mail470@gmail.com>
2024-01-31 16:28:45 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Framework\Router;
+use Framework\Session;
+
+Session::start();
+
+require '../helpers.php';
+
+
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
+
+// Instantiate the router
+$router = new Router();
+
+// Get routes
+$routes = require basePath('routes.php');
+
+// Get current URI and HTTP method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Route the request
+$router->route($uri);
